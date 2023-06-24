27b60ede4fa7f4ddb7eda7c87bfee2c1912c9f95
Ameer Hamza <ameer.hamza0046@gmail.com>
2023-06-24 14:10:07 +0530

    completed project

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const [emailLabel, passwordLabel] = document.querySelectorAll(".field > div")
-const [emailInput, passwordInput] = document.querySelectorAll(".field > input")
+const [emailLabel, passwordLabel] = document.querySelectorAll(".field > div");
+const [emailInput, passwordInput] = document.querySelectorAll(".field > input");
 
 const stringToSpanString = (txt) => {
-    let str = "";
-    [...txt].forEach(c => {
-        str += `<span>${c}</span>`;
-    })
-    return str;
-}
+  let str = "";
+  [...txt].forEach((c) => {
+    str += `<span>${c}</span>`;
+  });
+  return str;
+};
 
 emailLabel.innerHTML = stringToSpanString(emailLabel.innerHTML);
 passwordLabel.innerHTML = stringToSpanString(passwordLabel.innerHTML);
@@ -15,42 +15,50 @@ passwordLabel.innerHTML = stringToSpanString(passwordLabel.innerHTML);
 const emailLabelSpan = document.querySelectorAll(".email span");
 const passwordLabelSpan = document.querySelectorAll(".password span");
 
+const delay = 40;
+
 emailInput.addEventListener("focusin", () => {
-    let t = 0;
-    emailLabelSpan.forEach(span => {
-        setTimeout(() => {
-            span.classList.add("focused");
-        }, 0 + 100 * t);
-        t++;
-    })
-})
+  let t = 0;
+  emailLabelSpan.forEach((span) => {
+    setTimeout(() => {
+      span.classList.add("focused");
+    }, 0 + delay * t);
+    t++;
+  });
+});
 
 emailInput.addEventListener("focusout", () => {
-    let t = 0;
-    emailLabelSpan.forEach(span => {
-        setTimeout(() => {
-            span.classList.remove("focused");
-        }, 0 + 100 * t);
-        t++;
-    })
-})
+  if (emailInput.value !== "") {
+    return;
+  }
+  let t = 0;
+  emailLabelSpan.forEach((span) => {
+    setTimeout(() => {
+      span.classList.remove("focused");
+    }, delay * t);
+    t++;
+  });
+});
 
 passwordInput.addEventListener("focusin", () => {
-    let t = 0;
-    passwordLabelSpan.forEach(span => {
-        setTimeout(() => {
-            span.classList.add("focused");
-        }, 0 + 100 * t);
-        t++;
-    })
-})
+  let t = 0;
+  passwordLabelSpan.forEach((span) => {
+    setTimeout(() => {
+      span.classList.add("focused");
+    }, delay * t);
+    t++;
+  });
+});
 
 passwordInput.addEventListener("focusout", () => {
-    let t = 0;
-    passwordLabelSpan.forEach(span => {
-        setTimeout(() => {
-            span.classList.remove("focused");
-        }, 0 + 100 * t);
-        t++;
-    })
-})
+  if (passwordInput.value !== "") {
+    return;
+  }
+  let t = 0;
+  passwordLabelSpan.forEach((span) => {
+    setTimeout(() => {
+      span.classList.remove("focused");
+    }, delay * t);
+    t++;
+  });
+});
